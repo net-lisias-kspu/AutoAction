36bbc72eabf5d71cc3cb033960f99d94a08dd43e
formicant <formicant@gmail.com>
2016-05-29 21:15:37 +0600

    Tabs replaced with 4 spaces.

--- AutoAction/AGXInterface.cs
+++ AutoAction/AGXInterface.cs
@@ -8,24 +8,24 @@ using System.Reflection;
 
 namespace AutoAction
 {
-	class AGXInterface
-	{
-		public static bool AGExtInstalled()
-		{
-			try
-			{
-				Type calledType = Type.GetType("ActionGroupsExtended.AGExtExternal, AGExt");
-				return (bool)calledType.InvokeMember("AGXInstalled", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, null, null);
-			}
-			catch //if AGX not installed, above throws a null ref error, catch it
-			{
-				return false;
-			}
-		}
-		public static void AGExtToggleGroup(int group)
-		{
-			Type calledType = Type.GetType("ActionGroupsExtended.AGExtExternal, AGExt");
-			calledType.InvokeMember("AGXToggleGroup", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, null, new System.Object[] { group });
-		}
-	}
+    class AGXInterface
+    {
+        public static bool AGExtInstalled()
+        {
+            try
+            {
+                Type calledType = Type.GetType("ActionGroupsExtended.AGExtExternal, AGExt");
+                return (bool)calledType.InvokeMember("AGXInstalled", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, null, null);
+            }
+            catch //if AGX not installed, above throws a null ref error, catch it
+            {
+                return false;
+            }
+        }
+        public static void AGExtToggleGroup(int group)
+        {
+            Type calledType = Type.GetType("ActionGroupsExtended.AGExtExternal, AGExt");
+            calledType.InvokeMember("AGXToggleGroup", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, null, new System.Object[] { group });
+        }
+    }
 }
