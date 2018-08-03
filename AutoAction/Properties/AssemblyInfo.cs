fa84c546c7696e85f1634fa407a475f0c17b3c59
Lisias T <me@lisias.net>
2018-08-03 20:14:26 -0300

    Normalizing Versioning

--- AutoAction/Properties/AssemblyInfo.cs
+++ AutoAction/Properties/AssemblyInfo.cs
@@ -28,9 +28,10 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.10.3.0")]
+[assembly: AssemblyFileVersion(AutoAction.Version.Number)]
+[assembly: AssemblyVersion(AutoAction.Version.Number)]
 
 // Use KSPAssembly to allow other DLLs to make this DLL a dependency in a 
 // non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it 
 // does not appear to have a hard requirement to match the assembly version. 
-[assembly: KSPAssembly("AutoAction", 1, 10)]
+[assembly: KSPAssembly("AutoAction", AutoAction.Version.major, AutoAction.Version.minor)]
