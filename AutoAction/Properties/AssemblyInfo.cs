4e4bfaa6c04e1b58c3c3ff995f75ddf3533fa0e6
formicant <formicant@gmail.com>
2018-10-23 11:01:04 +0700

    Updated to KSP 1.5.1.

--- AutoAction/Properties/AssemblyInfo.cs
+++ AutoAction/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.10.3.0")]
+[assembly: AssemblyVersion("1.10.4.0")]
 
 // Use KSPAssembly to allow other DLLs to make this DLL a dependency in a 
 // non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it 
